ddad2aa8b7827f2be36d25af933f24d216c627d7
Shehzan Devani <shehzandevani@gmail.com>
2015-02-14 20:38:06 -0800

    Finished ken ken renderer

--- app.js
+++ app.js
@@ -5,7 +5,36 @@ var colorGrids = function(groups) {
   for (var id in groups) {
     $("." + id).css({background: randomColor()});
   }
-}
+};
+
+var groupLabel = function(group) {
+  if (!group.labelled) {
+    group.labelled = true;
+    return group.operation + " " + group.result;
+  }
+  return ".";
+};
+
+var notesLabel = function(cell) {
+  if (cell.notes) {
+    return cell.notes.join(", ");
+  } else {
+    return ".";
+  }
+};
+
+var constructCell = function(cell, groups) {
+  var cellText = $('<div>');
+  var groupText = groupLabel(groups[cell.group]);
+  cellText.append($("<p>").text(groupText));
+  cellText.append("<br>");
+  cellText.append($("<h3>").text(cell.number));
+  var notesText = notesLabel(cell);
+  cellText.append($("<p>").text(notesText));
+  var cellElem = $('<td>').append(cellText);
+  cellElem.addClass(cell.group);
+  return cellElem;
+};
 
 var constructGrid = function(data) {
   var grid = [];
@@ -17,8 +46,7 @@ var constructGrid = function(data) {
 
   data.cells.forEach(function(column, colNum) {
     column.forEach(function(cell, rowNum) {
-      var cellElem = $('<td>').text(cell.number);
-      cellElem.addClass(cell.group);
+      var cellElem = constructCell(cell, data.groups);
       grid[rowNum].append(cellElem);
     });
   });
@@ -31,9 +59,9 @@ var constructGrid = function(data) {
 };
 
 var randomColor = function() {
-  var n1 = Math.floor(Math.random() * 127) + 128;
-  var n2 = Math.floor(Math.random() * 127) + 128;
-  var n3 = Math.floor(Math.random() * 127) + 128;
+  var n1 = Math.floor(Math.random() * 75) + 180;
+  var n2 = Math.floor(Math.random() * 75) + 180;
+  var n3 = Math.floor(Math.random() * 75) + 180;
 
   return "rgb(" + n1 + "," + n2 + "," + n3 + ")";
 };
