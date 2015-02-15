7a6db51ad2064f321f3e710dc18686812c60cc4b
Shehzan Devani <shehzandevani@gmail.com>
2015-02-14 20:24:05 -0800

    Visualizing ken ken

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
 // first index is the column
 // second index is the row
 
-$.get('/samplefile.json', function(data) {
-  console.log(data);
-});
+var colorGrids = function(groups) {
+  for (var id in groups) {
+    $("." + id).css({background: randomColor()});
+  }
+}
+
+var constructGrid = function(data) {
+  var grid = [];
+
+  for (var i = 0; i < data.n; i++) {
+    var row = $('<tr>');
+    grid.push(row);
+  }
+
+  data.cells.forEach(function(column, colNum) {
+    column.forEach(function(cell, rowNum) {
+      var cellElem = $('<td>').text(cell.number);
+      cellElem.addClass(cell.group);
+      grid[rowNum].append(cellElem);
+    });
+  });
+
+  grid.forEach(function(elem) {
+    $('tbody').append(elem);
+  });
+
+  colorGrids(data.groups)
+};
+
+var randomColor = function() {
+  var n1 = Math.floor(Math.random() * 127) + 128;
+  var n2 = Math.floor(Math.random() * 127) + 128;
+  var n3 = Math.floor(Math.random() * 127) + 128;
+
+  return "rgb(" + n1 + "," + n2 + "," + n3 + ")";
+};
+
+$.get('/samplefile.json', constructGrid);
